733aaf1ff35fecdf7e60c926b7c67ee5c1d67f18
chrisfoehser2 <chrisfoehser2@gmail.com>
2023-01-20 20:34:33 -0500

    more updates

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -22,7 +22,7 @@ const Budget = () => {
 	
 	return (
 		<div className='alert alert-secondary'>
-			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		Budget:	$<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
 		</div>
 	);
 };
